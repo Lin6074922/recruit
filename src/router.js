777ff34ce22620309c3f64621a55dff001804b54
lqx <lin6074922@163.com>
2019-04-26 13:27:39 +0800

    1

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
-import Home from './components/Home'
+import Home from './views/Home'
 import Details from './components/Details'
 import com from './components/Company'
 import job from './components/joblist'
@@ -19,18 +19,5 @@ export default new Router({
     {path:'/com',name:'company',component:com},
     {path:'/company',name:'companyList',component:companyList},
     {path:'/job',name:'jobList',component:job},
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // },
   ]
 })
